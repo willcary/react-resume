0afb5b91e3853616664df9d5a2206e3f614d8779
willcary <w.a.c.ary@gmail.com>
2022-03-09 16:27:55 -0500

    Import Git-Cert and Adjust to className

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-
+import gitImage from "./images/Cary-Git-Certificate.pdf"
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div class="header-container">
+        <div className="header-container">
           <h1>William Cary</h1>
-          <p class="subtitle">Front-End Developer</p>
+          <p className="subtitle">Front-End Developer</p>
         </div>
       </header>
 
       <main>
-        <div class="main-container">
+        <div className="main-container">
           <p>Motivated front-end developer with experience designing and developing responsive websites and applications, pivoting from a background in science and education. An eager learner with strong communication skills and an affinity for solving problems.</p>
 
           {/* <!--Work History--> */}
-          <h2 class="accent-gradient">Work History</h2>
-          <div class="card">
-            <div class="card-main">
+          <h2 className="accent-gradient">Work History</h2>
+          <div className="card">
+            <div className="card-main">
               <h3>Freelance Web Developer</h3>
               <h4>September 2018 - Present</h4>
             </div>
@@ -32,12 +32,12 @@ function App() {
               <li>Redesigned and improved responsive layouts for select pages.</li>
             </ul>
           </div>
-          <div class="card">
-            <div class="card-main">
+          <div className="card">
+            <div className="card-main">
               <h3>Teacher</h3>
               <h4>August 2018 - August 2021</h4>
             </div>
-            <div class="card-content">
+            <div className="card-content">
               <p><em>Montessori School of Syracuse - Syracuse, NY</em></p>
               <ul>
                 <li>Developed an extensive curriculum to teach 4th, 5th, and 6th graders how to code using Scratch, HTML, CSS, and other programs.</li>
@@ -47,75 +47,75 @@ function App() {
           </div>
 
           {/* <!--Education--> */}
-          <h2 class="accent-gradient">Education</h2>
-          <div class="card">
-            <div class="card-main">
-              <h3>Master of Science<span class="font-regular"> - Geology</span></h3>
+          <h2 className="accent-gradient">Education</h2>
+          <div className="card">
+            <div className="card-main">
+              <h3>Master of Science<span className="font-regular"> - Geology</span></h3>
               <h4>June 2018</h4>
             </div>
-            <div class="card-details">
+            <div className="card-details">
               <p><em>Western Washington University - Bellingham, WA</em></p>
               <p>GPA: 3.9</p>
             </div>
           </div>
-          <div class="card">
-            <div class="card-main">
-              <h3>Bachelor of Arts<span class="font-regular"> - Geology</span></h3>
+          <div className="card">
+            <div className="card-main">
+              <h3>Bachelor of Arts<span className="font-regular"> - Geology</span></h3>
               <h4>May 2013</h4>
             </div>
-            <div class="card-details">
+            <div className="card-details">
               <p><em>The College of Wooster - Wooster, OH</em></p>
               <p>GPA: 3.6</p>
             </div>
           </div>
 
           {/* <!--Certificates--> */}
-          <h2 class="accent-gradient">Certificates</h2>
-          <div class="cert-card">
-            <div class="cert-card-content">
-              <h3><a href="https://drive.google.com/file/d/1cXlqo4zK0Cw3iml4mVklS1cnia4MMuue/view?usp=sharing" target="_blank">The Ultimate Git Course</a></h3>
+          <h2 className="accent-gradient">Certificates</h2>
+          <div className="cert-card">
+            <div className="cert-card-content">
+              <h3><a href={gitImage} target="_blank">The Ultimate Git Course</a></h3>
               <p><em>Code with Mosh</em></p>
             </div>
             <h4>March 2022</h4>
           </div>
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://scrimba.com/certificate/uyeqdyTQ/greact" target="_blank">The React Bootcamp</a></h3>
               <p><em>Scrimba</em></p>
             </div>
             <h4>September 2021</h4>
           </div>
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://www.hackerrank.com/certificates/65b269a5e228" target="_blank">CSS</a></h3>
               <p><em>HackerRank</em></p>
             </div>
             <h4>September 2021</h4>
           </div>
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://scrimba.com/certificate/uyeqdyTQ/gresponsive" target="_blank">The Responsive Web Design Bootcamp</a></h3>
               <p><em>Scrimba</em></p>
             </div>
             <h4>August 2021</h4>
           </div>
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://scrimba.com/certificate/uyeqdyTQ/gjavascript" target="_blank">The JavaScript Bootcamp</a></h3>
               <p><em>Scrimba</em></p>
             </div>
             <h4>July 2020</h4>
           </div>
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://freecodecamp.org/certification/willcary/javascript-algorithms-and-data-structures" target="_blank">JavaScript Algorithms and Data Structures</a></h3>
               <p><em>freeCodeCamp</em></p>
             </div>
             <h4>January 2019</h4>
           </div>
 
-          <div class="cert-card">
-            <div class="cert-card-content">
+          <div className="cert-card">
+            <div className="cert-card-content">
               <h3><a href="https://freecodecamp.org/certification/willcary/responsive-web-design" target="_blank">Responsive Web Design</a></h3>
               <p><em>freeCodeCamp</em></p>
             </div>
@@ -123,46 +123,46 @@ function App() {
           </div>
         </div>
 
-        <aside class="aside-container">
+        <aside className="aside-container">
           {/* <!--Skills--> */}
-          <section class="skills">
-            <h2 class="accent-gradient">Skills</h2>
-            <div class="skill-box">
-              <div class="skill">HTML</div>
-              <div class="skill">CSS</div>
-              <div class="skill">JavaScript</div>
-              <div class="skill">React</div>
-              <div class="skill">Responsive Design</div>
-              <div class="skill">Bootstrap</div>
-              <div class="skill">SCSS</div>
-              <div class="skill">Git</div>
-              <div class="skill">HTML5</div>
-              <div class="skill">ES6</div>
+          <section className="skills">
+            <h2 className="accent-gradient">Skills</h2>
+            <div className="skill-box">
+              <div className="skill">HTML</div>
+              <div className="skill">CSS</div>
+              <div className="skill">JavaScript</div>
+              <div className="skill">React</div>
+              <div className="skill">Responsive Design</div>
+              <div className="skill">Bootstrap</div>
+              <div className="skill">SCSS</div>
+              <div className="skill">Git</div>
+              <div className="skill">HTML5</div>
+              <div className="skill">ES6</div>
             </div>
           </section>
 
           {/* <!--Contact--> */}
-          <section class="contact">
-            <h2 class="accent-gradient">Contact</h2>
-            <div class="contact-info">
+          <section className="contact">
+            <h2 className="accent-gradient">Contact</h2>
+            <div className="contact-info">
               <p><strong>Email</strong></p>
               <p><a href="mailto:w.a.c.ary@gmail.com" target="_blank">w.a.c.ary@gmail.com</a></p>
             </div>
-            <div class="contact-info">
+            <div className="contact-info">
               <p><strong>Phone</strong></p>
               <p>(330) 466-6693</p>
             </div>
-            <div class="contact-info">
+            <div className="contact-info">
               <p><strong>Address</strong></p>
               <p>Carrboro, NC, 27510</p>
             </div>
-            <div class="contact-info">
+            <div className="contact-info">
               <p><strong>Linkedin</strong></p>
-              <p class="link-wrap"><a href="https://www.linkedin.com/in/william-cary/" target="_blank">Click for Profile</a></p>
+              <p className="link-wrap"><a href="https://www.linkedin.com/in/william-cary/" target="_blank">Click for Profile</a></p>
             </div>
-            <div class="contact-info">
+            <div className="contact-info">
               <p><strong>Website</strong></p>
-              <p class="link-wrap"><a href="https://www.williamacary.com/" target="_blank">www.williamacary.com</a></p>
+              <p className="link-wrap"><a href="https://www.williamacary.com/" target="_blank">www.williamacary.com</a></p>
             </div>
           </section>
 
