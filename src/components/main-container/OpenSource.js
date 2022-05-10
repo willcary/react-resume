758272e0c8e4172bce13067a8dccbf1b86414121
willcary <w.a.c.ary@gmail.com>
2022-05-10 10:19:10 -0400

    Add OpenSource Component

---
+++ src/components/main-container/OpenSource.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function OpenSource() {
+  return (
+    <section>
+      <h2 className='accent-gradient'>Open Source</h2>
+      <div className='card'>
+        <div className='card-main'>
+          <h3>Contributor</h3>
+          <h4>March 2022 - Present</h4>
+        </div>
+        <p>
+          <em>
+            <a href='https://github.com/wijohnst/thriving-park'>
+              Thriving Park
+            </a>{' '}
+            and{' '}
+            <a href='https://github.com/wijohnst/thriving-park-documentation'>
+              Thriving Park Documentation
+            </a>{' '}
+            - Columbus, OH
+          </em>
+        </p>
+        <ul>
+          <li>
+            Develop and refactor react components, update documentation, fix
+            bugs, and complete code reviews.
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
