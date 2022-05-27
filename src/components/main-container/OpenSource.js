0f8c4a7a6c78fbedc27acd8f9b37c1b43fe09bf0
willcary <w.a.c.ary@gmail.com>
2022-05-27 14:01:23 -0400

    Edit OpenSource component

--- src/components/main-container/OpenSource.js
+++ src/components/main-container/OpenSource.js
@@ -3,19 +3,19 @@ import React from 'react'
 export default function OpenSource() {
   return (
     <section>
-      <h2 className='accent-gradient'>Open Source</h2>
-      <div className='card'>
-        <div className='card-main'>
+      <h2 className="accent-gradient">Open Source</h2>
+      <div className="card">
+        <div className="card-main">
           <h3>Contributor</h3>
           <h4>March 2022 - Present</h4>
         </div>
         <p>
           <em>
-            <a href='https://github.com/wijohnst/thriving-park'>
+            <a href="https://github.com/wijohnst/thriving-park">
               Thriving Park
             </a>{' '}
             and{' '}
-            <a href='https://github.com/wijohnst/thriving-park-documentation'>
+            <a href="https://github.com/wijohnst/thriving-park-documentation">
               Thriving Park Documentation
             </a>{' '}
             - Columbus, OH
@@ -23,9 +23,12 @@ export default function OpenSource() {
         </p>
         <ul>
           <li>
-            Develop and refactor react components, update documentation, fix
-            bugs, and complete code reviews for the Driving Park Neighborhood
-            community portal.
+            Develop and refactor react components using React,Typescript, React
+            Testing Library, Storybook, and Styled-Components.
+          </li>
+          <li>
+            Update documentation, fix bugs, and complete code reviews for the
+            Driving Park Neighborhood community portal.
           </li>
         </ul>
       </div>
